59ea38768dbbe848b34dff846b85d642c21b4d60
Angélica Silva <angelica.silva9323@gmail.com>
2021-12-02 19:15:35 -0300

    Adicionado comentários.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Função para capturar o email no input.
 function getEmail() {
   const email = document.querySelector('#email-login').value;
   return email;
 }
 
+// Função para capturar a senha no input.
 function getPassword() {
   const password = document.querySelector('#password').value;
   return password;
 }
 
+// Função para validar o login.
+// ref.: https://stackoverflow.com/questions/13831601/disabling-and-enabling-a-html-input-button
 function validateLogin(email, password) {
   if (email === 'tryber@teste.com' && password === '123456') {
     return true;
@@ -15,6 +19,7 @@ function validateLogin(email, password) {
   return false;
 }
 
+// Função para emitir alertas conforme resposta da validação.
 function alertLogin() {
   if (validateLogin(getEmail(), getPassword()) === true) {
     alert('Olá, Tryber!');
@@ -23,13 +28,14 @@ function alertLogin() {
   }
 }
 
+// Função que adiciona o Event Listener ao botão de entrar.
 function btnLogin() {
   const buttonLogin = document.querySelector('#login');
   buttonLogin.addEventListener('click', alertLogin);
 }
 
 // ----
-
+// Função que valida se marcou o checkbox.
 function validateCheck() {
   const agreement = document.querySelector('#agreement');
   if (agreement.checked) {
@@ -38,6 +44,7 @@ function validateCheck() {
   return false;
 }
 
+// Função que habilita ou desabilita o botão de enviar conforme marcação do checkbox.
 function submit() {
   const button = document.querySelector('#submit-btn');
   if (validateCheck() === true) {
@@ -47,23 +54,26 @@ function submit() {
   }
 }
 
+// Função que adiciona o Event Listener ao botão de enviar.
 function checkSubmit() {
   const checkAgreement = document.querySelector('#agreement');
   checkAgreement.addEventListener('click', submit);
 }
 
 // ----
-
+// Função que caputa a quantidade de caracteres inseridos na text area.
 function getTextChars() {
   const textArea = document.querySelector('#textarea').value;
   return textArea.length;
 }
 
+// Função que insere no elemento #counter a quantidade de caracteres restantes (max. 500).
 function countChar() {
   const textChars = getTextChars();
   document.querySelector('#counter').innerText = (500 - textChars);
 }
 
+// Função que adiciona o Event Listener à text area conforme a pessoa digite.
 function charsCount() {
   const textArea = document.querySelector('#textarea');
   textArea.addEventListener('keyup', countChar);
